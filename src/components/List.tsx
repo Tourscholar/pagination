aecefd29858f7761978e13a0f2336c6bfe9a316c
Tourscholar <krj1608178365@gmail.com>
2022-03-05 23:04:36 +0800

    fix: move list display logic to parent component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,16 +1,13 @@
-import { lists } from '@/utils/db';
-import { useEffect, useState } from 'react';
-
-export const List = (props: { onCurrent: number }) => {
-  const { onCurrent } = props;
-  const [dataSource, setLists] = useState(lists[onCurrent - 1]);
-
-  useEffect(() => {
-    setLists(lists[onCurrent - 1]);
-  }, [onCurrent]);
-  const list = dataSource.map((show, key) => {
-    return <h1 key={key}>{show.name}</h1>;
+export const List = (props: {
+  dataSource: {
+    id: number;
+    name: string;
+  }[];
+}) => {
+  const { dataSource } = props;
+  const lists = dataSource.map((list, key) => {
+    return <h1 key={list.id}>{list.name}</h1>;
   });
 
-  return <>{list}</>;
+  return <>{lists}</>;
 };
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,6 @@
+import { lists } from '@/utils/db';
 import { Props } from '@/utils/Props';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from './Button';
 import { List } from './List';
 import { Pagers } from './Pargers';
@@ -13,10 +14,15 @@ export const Pagination = (props: Props) => {
   const { totalPages, defaultCurrent } = props;
 
   const [current, setPage] = useState(defaultCurrent);
+  const [dataSource, setLists] = useState(lists[current - 1]);
+
+  useEffect(() => {
+    setLists(lists[current - 1]);
+  }, [current]);
 
   return (
     <>
-      <List onCurrent={current} />
+      <List dataSource={dataSource}/>
       <div className="m-pagination">
         <Button
           className="btn-prev"
